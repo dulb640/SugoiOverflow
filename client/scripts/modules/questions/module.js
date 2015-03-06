ef4498fefa87f7cc00855c0bf5f69a455e9e0c99
Sandy987 <sanchit.uttam987@gmail.com>
2015-03-06 16:05:39 +1300

    Fix broken merge

--- client/scripts/modules/questions/module.js
+++ client/scripts/modules/questions/module.js
@@ -4,9 +4,6 @@ angular.module('sugoiOverflow.questions', ['sugoiOverflow.shared', 'sugoiOverflo
       'use strict';
 
       $stateProvider
-        .state('root.newQuestion', {
-          url: '/newQuestion',
-          templateUrl: '/views/questions/newQuestion.html',
         .state('root.questions.new', {
           url: 'questions/new',
           templateUrl: 'views/questions/newQuestion.html',
