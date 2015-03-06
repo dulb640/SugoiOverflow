e1e8307a48d545aabb4b7314ded89c13471ad47b
Sandy987 <sanchit.uttam987@gmail.com>
2015-03-06 14:16:26 +1300

    Added profile stuff

--- client/scripts/modules/profile/controllers/profileDialogController.js
+++ client/scripts/modules/profile/controllers/profileDialogController.js
@@ -1,14 +1,20 @@
 angular.module('sugoiOverflow.profile')
-  .controller('profileController', [
-      '$scope',
-      '$q',
-    function($scope, $q){
+  .controller('profileController',
+    function($scope, $q, tagsDataService, userDataService){
       'use strict'
 
       _.extend($scope, {
+        user: {},
         tags: [],
-        loadTags: function($query){return [];},
+        loadTags: function($query){return tagsDataService.getAvailableTags($query);},
         submit: function(){}
       });
+
+      userDataService.getUser('1234')
+      .then(function(user){
+        $scope.user = user;
+      })
+      .catch(function(error){
+      });
     }
-  ]);
+  );
