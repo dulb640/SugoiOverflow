893c74ca33b566fa6035b1bce9359c4ad6fc063b
Sandy987 <sanchit.uttam987@gmail.com>
2015-03-06 16:20:19 +1300

    Started mocking services

--- client/scripts/modules/shared/services/tagsDataService.js
+++ client/scripts/modules/shared/services/tagsDataService.js
@@ -3,15 +3,17 @@ angular.module('sugoiOverflow')
   function($http, $q){
     'use strict';
     var service = {
-      getAvailableTags: function(query){
+      getAvailableTags: function(){
         var deferred = $q.defer();
-        $http.get('/api/tags/' + query)
-        .success(function(data){
-          deferred.resolve(data);
-        })
-        .error(function(error){
-          deferred.reject(error);
-        });
+        // $http.get('/api/tags/' + query)
+        // .success(function(data){
+        //   deferred.resolve(data);
+        // })
+        // .error(function(error){
+        //   deferred.reject(error);
+        // });
+
+        deferred.resolve([{text: 'Tag 1'}, {text: 'Tag 2'}]);
         return deferred.promise;
       }
     };
--- client/scripts/modules/shared/services/userDataService.js
+++ client/scripts/modules/shared/services/userDataService.js
@@ -5,14 +5,24 @@ angular.module('sugoiOverflow.shared')
       var service = {
         getUser: function(userId){
           var deferred = $q.defer();
-          $http.get('/api/user/' + userId)
-          .success(function(data){
-            deferred.resolve(data);
-          })
-          .error(function(error){
-            deferred.reject(error);
+          // $http.get('/api/user/' + userId)
+          // .success(function(data){
+          //   deferred.resolve(data);
+          // })
+          // .error(function(error){
+          //   deferred.reject(error);
+          // });
+          deferred.resolve({
+            userId: userId,
+            name: 'jack',
+            karma: 99,
+            location: 'Water Cooler',
+            tags: [{text: 'hello'}]
           });
           return deferred.promise;
+        },
+        saveUserDetails: function(userId, user){
+
         }
       };
 
