be030cb010a5fa1f70ec21a9351c73106cd2eac2
Deadarius <deadarius.dendr@gmail.com>
2015-03-08 14:06:30 +1300

    Ask question preview with syntax highlight

--- client/scripts/filters/markdown.js
+++ client/scripts/filters/markdown.js
@@ -1,9 +1,17 @@
-angular.module('sugoiOverflow.filters').filter('markdown', function($sanitize) {
+/*angular.module('sugoiOverflow.filters').filter('markdown', function($sanitize) {
   'use strict';
+  marked.setOptions({
+    sanitize: true,
+    gfm: true,
+    breaks: true,
+    highlight: function (code) {
+      return hljs.highlightAuto(code).value;
+    }
+  });
 
   return function(input) {
     var markdown = marked(input);
-    var sanitized = $sanitize(markdown);
-    return sanitized;
+    return markdown;
   };
 });
+*/
