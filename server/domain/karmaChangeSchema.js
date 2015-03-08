7c365959590f0bf0594888cb0bf18b4d45e8189d
Deadarius <deadarius.dendr@gmail.com>
2015-03-09 11:00:27 +1300

    Implemented karma

---
+++ server/domain/karmaChangeSchema.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var mongoose = require('mongoose-q')(require('mongoose'));
+var Schema = mongoose.Schema;
+
+var KarmaChange = new Schema({
+  question:{
+    type: Schema.Types.ObjectId,
+    ref: 'Question'
+  },
+  timestamp:{
+    type: Date,
+    'default': Date.now
+  },
+  reason:{
+    type: String,
+    required: true
+  },
+  value:{
+    type: Number,
+    required: true
+  }
+});
+
+KarmaChange.set('toJSON', {
+  transform: function (doc, ret) {
+    ret.id = ret._id;
+    delete ret._id;
+    delete ret.__v;
+  }
+});
+module.exports = KarmaChange;
