b49599b153963d02cd32970accc2b45495b6a21f
Deadarius <deadarius.dendr@gmail.com>
2015-03-11 02:06:34 +1300

    Added support for yaml configuration #25 and added example config #20

--- server/configuration.js
+++ server/configuration.js
@@ -2,18 +2,20 @@
 
 var nconf = require('nconf');
 var path  = require('path');
-
+var yaml = require('nconf-yaml');
 nconf.argv()
   .env();
 
 var environment = nconf.get('NODE_ENV');
 if(environment){
   nconf
-  .file({ file: path.join(__dirname, '../config.' + environment + '.json') });
+  .file({ file: path.join(__dirname, '../config.' + environment + '.json') })
+  .file({ file: path.join(__dirname, '../config.' + environment + '.yaml'), format: yaml });
 }
 
 nconf
   .file({ file: path.join(__dirname, '../config.json') })
+  .file({ file: path.join(__dirname, '../config.yaml'), format: yaml })
   .use('memory')
   .defaults({
     'if nothing else': 'use this value'
