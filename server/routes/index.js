aabb9d1a53f41c4a245766ad825f203149aa6d75
Deadarius <deadarius.dendr@gmail.com>
2015-03-17 22:25:35 +1300

    Added bower to postinstall script

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,12 +2,14 @@
 
 var express = require('express');
 var passport = require('passport');
+var secure = require('./secure');
+
 
 var router  = express.Router();
 router.use('/auth', 		                                                require('./authRoutes'));
 router.use('/files', 	   /*passport.authenticate('jwt', { session: false}),*/ require('./filesRoutes'));
 
-router.use('/questions',   passport.authenticate('jwt', { session: false}), require('./questionsRoutes'));
+router.use('/questions',   secure(), require('./questionsRoutes'));
 router.use('/profiles',    passport.authenticate('jwt', { session: false}), require('./profilesRoutes'));
 router.use('/tags',        passport.authenticate('jwt', { session: false}), require('./tagsRoutes'));
 router.use('/suggestions', passport.authenticate('jwt', { session: false}), require('./suggestionsRoutes'));
