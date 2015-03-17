067f0de2af5d31eadd9e8d7ffaf01920b69de0dc
Deadarius <deadarius.dendr@gmail.com>
2015-03-17 22:40:22 +1300

    Removed postinstall script

--- server/routes/index.js
+++ server/routes/index.js
@@ -9,7 +9,7 @@ var router  = express.Router();
 router.use('/auth', 		                                                require('./authRoutes'));
 router.use('/files', 	   /*passport.authenticate('jwt', { session: false}),*/ require('./filesRoutes'));
 
-router.use('/questions',   secure(), require('./questionsRoutes'));
+router.use('/questions',   passport.authenticate('jwt', { session: false}), require('./questionsRoutes'));
 router.use('/profiles',    passport.authenticate('jwt', { session: false}), require('./profilesRoutes'));
 router.use('/tags',        passport.authenticate('jwt', { session: false}), require('./tagsRoutes'));
 router.use('/suggestions', passport.authenticate('jwt', { session: false}), require('./suggestionsRoutes'));
