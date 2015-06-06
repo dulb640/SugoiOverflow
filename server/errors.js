1330ba3e66312ff265542e19238a115b2779d299
Deadarius <deadarius.dendr@gmail.com>
2015-06-06 23:05:42 +1200

    Added new ArgumentError as well as proper stack handling in custom errors

--- server/errors.js
+++ server/errors.js
@@ -3,6 +3,7 @@
 function NotFoundError(message) {
   this.name = 'NotFoundError';
   this.message = (message || '');
+  this.stack = new Error().stack;
 }
 NotFoundError.prototype = Error.prototype;
 
@@ -10,10 +11,20 @@ function GenericError(message, inner) {
   this.name = 'GenericError';
   this.message = (message || '');
   this.inner = inner;
+  this.stack = new Error().stack;
 }
 GenericError.prototype = Error.prototype;
 
+function ArgumentError(message, argumentName) {
+  this.name = 'ArgumentError';
+  this.argumentName = argumentName;
+  this.message = (message || '');
+  this.stack = new Error().stack;
+}
+ArgumentError.prototype = Error.prototype;
+
 module.exports = {
   NotFoundError: NotFoundError,
-  GenericError: GenericError
+  GenericError: GenericError,
+  ArgumentError: ArgumentError
 };
