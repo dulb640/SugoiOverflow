7d875b5353e091c5b53ea13334e03eff133988b6
Deadarius <deadarius.dendr@gmail.com>
2015-06-07 16:45:44 +1200

    Added missing controller

---
+++ client/scripts/shared/rootController.js
@@ -0,0 +1,6 @@
+angular.module('sugoiOverflow.shared').controller('rootController', function($scope, $rootScope){
+  'use strict';
+  $rootScope.$on('titleChanged', function(event, newTitle){
+    $scope.title = newTitle;
+  });
+});
