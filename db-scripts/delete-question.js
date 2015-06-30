5b53e4fed5bb89173c5cc6b842f9752d47fdaadb
Deadarius <deadarius.dendr@gmail.com>
2015-06-30 23:43:50 +1200

    Updated styles for db-scripts

--- db-scripts/delete-question.js
+++ db-scripts/delete-question.js
@@ -1,80 +1,80 @@
-'use strict';
-var assert = require('assert');
-var minimist = require('minimist');
-var _ = require('lodash');
-var Promise = require('bluebird');
-var MongoDB = require('mongodb');
-var MongoClient = MongoDB.MongoClient;
-Promise.promisifyAll(MongoDB);
-Promise.promisifyAll(MongoClient);
+'use strict'
+var assert = require('assert')
+var minimist = require('minimist')
+var _ = require('lodash')
+var Promise = require('bluebird')
+var MongoDB = require('mongodb')
+var MongoClient = MongoDB.MongoClient
+Promise.promisifyAll(MongoDB)
+Promise.promisifyAll(MongoClient)
 
-var argv = minimist(process.argv.slice(2));
-assert(argv.url, 'Url for mongodb is not provided');
-assert(argv.id, 'Question\'s id is not specified');
-var id = new MongoDB.ObjectID(argv.id);
+var argv = minimist(process.argv.slice(2))
+assert(argv.url, 'Url for mongodb is not provided')
+assert(argv.id, 'Question\'s id is not specified')
+var id = new MongoDB.ObjectID(argv.id)
 
 var connection = MongoClient.connectAsync(argv.url)
-  .then(function(db) {
-    console.log('Connection established');
-    return db;
+  .then(function (db) {
+    console.log('Connection established')
+    return db
   })
-  .error(function(err) {
-    console.error(err);
+  .error(function (err) {
+    console.error(err)
   })
   .disposer(function (db) {
-    console.log('Closing connection...');
-    db.close();
-    console.log('Closed');
-    process.exit(0);
-  });
+    console.log('Closing connection...')
+    db.close()
+    console.log('Closed')
+    process.exit(0)
+  })
 
 Promise.using(connection, function (db) {
-  db.collection('questions').remove({_id: id}, { justOne: true });
-  var users = db.collection('users');
-  var userfeeds = db.collection('userfeeds');
+  db.collection('questions').remove({_id: id}, { justOne: true })
+  var users = db.collection('users')
+  var userfeeds = db.collection('userfeeds')
 
-  return users.find({'profile.asked': {$elemMatch : { $eq: id}}})
-    .map(function(u) {
-      console.log('Processing asked for user %s', u._id);
-      u.profile.asked = _.filter(u.profile.asked, function(s) {
-        return s && !s.equals(id);
-      });
-      return users.saveAsync(u);
+  return users.find({'profile.asked': { $elemMatch: { $eq: id} } })
+    .map(function (u) {
+      console.log('Processing asked for user %s', u._id)
+      u.profile.asked = _.filter(u.profile.asked, function (s) {
+        return s && !s.equals(id)
+      })
+      return users.saveAsync(u)
     })
     .toArrayAsync()
-    .then(function(promises) {
-      return Promise.all(promises);
+    .then(function (promises) {
+      return Promise.all(promises)
     })
-    .then(function() {
-      return users.find({'profile.answered': {$elemMatch : { $eq: id}}})
-        .map(function(u) {
-          console.log('Processing answered for user %s', u._id);
-          u.profile.answered = _.filter(u.profile.answered, function(s) {
-            return s && !s.equals(id);
-          });
+    .then(function () {
+      return users.find({'profile.answered': { $elemMatch: { $eq: id } } })
+        .map(function (u) {
+          console.log('Processing answered for user %s', u._id)
+          u.profile.answered = _.filter(u.profile.answered, function (s) {
+            return s && !s.equals(id)
+          })
 
-          return users.saveAsync(u);
+          return users.saveAsync(u)
         })
-        .toArrayAsync();
+        .toArrayAsync()
     })
-    .then(function(promises) {
-      return Promise.all(promises);
+    .then(function (promises) {
+      return Promise.all(promises)
     })
-    .then(function() {
-      return users.find({'profile.subscribed': {$elemMatch : { $eq: id}}})
-        .map(function(u) {
-          console.log('Processing subscribed for user %s', u._id);
-          u.profile.subscribed = _.filter(u.profile.subscribed, function(s) {
-            return s && !s.equals(id);
-          });
-          return users.saveAsync(u);
+    .then(function () {
+      return users.find({'profile.subscribed': { $elemMatch: { $eq: id } } })
+        .map(function (u) {
+          console.log('Processing subscribed for user %s', u._id)
+          u.profile.subscribed = _.filter(u.profile.subscribed, function (s) {
+            return s && !s.equals(id)
+          })
+          return users.saveAsync(u)
         })
-        .toArrayAsync();
+        .toArrayAsync()
     })
-    .then(function(promises) {
-      return Promise.all(promises);
+    .then(function (promises) {
+      return Promise.all(promises)
     })
-    .then(function() {
+    .then(function () {
       return users.find({
         'profile.karmaChanges': {
             $elemMatch : {
@@ -84,41 +84,41 @@ Promise.using(connection, function (db) {
             }
           }
         })
-        .map(function(u) {
-          console.log('Processing karmaChanges for user %s', u._id);
-          u.profile.karmaChanges = _.filter(u.profile.karmaChanges, function(k) {
-            return k && k.question && !k.question.equals(id);
-          });
-          return users.saveAsync(u);
+        .map(function (u) {
+          console.log('Processing karmaChanges for user %s', u._id)
+          u.profile.karmaChanges = _.filter(u.profile.karmaChanges, function (k) {
+            return k && k.question && !k.question.equals(id)
+          })
+          return users.saveAsync(u)
         })
-        .toArrayAsync();
+        .toArrayAsync()
     })
-    .then(function(promises) {
-      return Promise.all(promises);
+    .then(function (promises) {
+      return Promise.all(promises)
     })
-    .then(function() {
+    .then(function () {
       return userfeeds.find({
         questionNotifications: {
-          $elemMatch : {
+          $elemMatch: {
             question: {
               $eq: id
             }
           }
         }
       })
-      .map(function(f) {
-        console.log('Processing feed %s', f._id);
-        f.questionNotifications = _.filter(f.questionNotifications, function(n) {
-          return n && n.question && !n.question.equals(id);
-        });
-        return userfeeds.saveAsync(f);
+      .map(function (f) {
+        console.log('Processing feed %s', f._id)
+        f.questionNotifications = _.filter(f.questionNotifications, function (n) {
+          return n && n.question && !n.question.equals(id)
+        })
+        return userfeeds.saveAsync(f)
       })
-      .toArrayAsync();
+      .toArrayAsync()
     })
-    .then(function(promises) {
-      return Promise.all(promises);
+    .then(function (promises) {
+      return Promise.all(promises)
     })
-    .error(function(err) {
-      console.error(err);
-    });
-});
+    .error(function (err) {
+      console.error(err)
+    })
+})
--- db-scripts/rename-tags-to-param-case.js
+++ db-scripts/rename-tags-to-param-case.js
@@ -1,66 +1,66 @@
-'use strict';
-var assert = require('assert');
-var changeCase = require('change-case');
-var minimist = require('minimist');
-var _ = require('lodash');
-var Promise = require('bluebird');
-var MongoDB = require('mongodb');
-var MongoClient = MongoDB.MongoClient;
-Promise.promisifyAll(MongoDB);
-Promise.promisifyAll(MongoClient);
+'use strict'
+var assert = require('assert')
+var changeCase = require('change-case')
+var minimist = require('minimist')
+var _ = require('lodash')
+var Promise = require('bluebird')
+var MongoDB = require('mongodb')
+var MongoClient = MongoDB.MongoClient
+Promise.promisifyAll(MongoDB)
+Promise.promisifyAll(MongoClient)
 
-var argv = minimist(process.argv.slice(2));
-assert(argv.url, 'Url for mongodb is not provided');
+var argv = minimist(process.argv.slice(2))
+assert(argv.url, 'Url for mongodb is not provided')
 var connection = MongoClient.connectAsync(argv.url)
-  .then(function(db) {
-    console.log('Connection established');
-    return db;
+  .then(function (db) {
+    console.log('Connection established')
+    return db
   })
-  .error(function(err) {
-    console.error(err);
+  .error(function (err) {
+    console.error(err)
   })
   .disposer(function (db) {
-    console.log('Closing connection...');
-    db.close();
-    console.log('Closed');
-    process.exit(0);
-  });
+    console.log('Closing connection...')
+    db.close()
+    console.log('Closed')
+    process.exit(0)
+  })
 
 Promise.using(connection, function (db) {
-  console.log('Start renaming tags for questions');
-  var questions = db.collection('questions');
-  var questionsCursor = questions.find({tags: {$not: {$size: 0}}});
-  return questionsCursor.map(function(q) {
-    console.log('Processing question %s', q._id);
-    var tags = _.map(q.tags, function(t) {
-      return changeCase.paramCase(t);
-    });
-    console.log('%s -> %s', q.tags, tags);
-    q.tags = tags;
-    return questions.saveAsync(q);
+  console.log('Start renaming tags for questions')
+  var questions = db.collection('questions')
+  var questionsCursor = questions.find({tags: {$not: {$size: 0}}})
+  return questionsCursor.map(function (q) {
+    console.log('Processing question %s', q._id)
+    var tags = _.map(q.tags, function (t) {
+      return changeCase.paramCase(t)
+    })
+    console.log('%s -> %s', q.tags, tags)
+    q.tags = tags
+    return questions.saveAsync(q)
   })
   .toArrayAsync()
-  .then(function(promises) {
-    return Promise.all(promises);
+  .then(function (promises) {
+    return Promise.all(promises)
   })
-  .then(function() {
-    var users = db.collection('users');
-    var usersCursor = users.find({'profile.selectedTags': {$not: {$size: 0}}});
-    return usersCursor.map(function(u) {
-      console.log('Processing user %s', u._id);
-      var tags = _.map(u.profile.selectedTags, function(t) {
-        return changeCase.paramCase(t);
-      });
-      console.log('%s -> %s', u.profile.selectedTags, tags);
-      u.profile.selectedTags = tags;
-      return users.saveAsync(u);
+  .then(function () {
+    var users = db.collection('users')
+    var usersCursor = users.find({'profile.selectedTags': {$not: {$size: 0}}})
+    return usersCursor.map(function (u) {
+      console.log('Processing user %s', u._id)
+      var tags = _.map(u.profile.selectedTags, function (t) {
+        return changeCase.paramCase(t)
+      })
+      console.log('%s -> %s', u.profile.selectedTags, tags)
+      u.profile.selectedTags = tags
+      return users.saveAsync(u)
     })
-    .toArrayAsync();
+    .toArrayAsync()
   })
-  .then(function(promises) {
-    return Promise.all(promises);
+  .then(function (promises) {
+    return Promise.all(promises)
   })
-  .error(function(err) {
-    console.error(err);
-  });
-});
+  .error(function (err) {
+    console.error(err)
+  })
+})
