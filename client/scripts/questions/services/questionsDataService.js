fd4c07b623d4a4433c698a5a88b2a51ae77703f3
Deadarius <deadarius.dendr@gmail.com>
2015-07-23 21:58:21 +1200

    Introduced new angular service that transformes  api into promises

--- client/scripts/questions/services/questionsDataService.js
+++ client/scripts/questions/services/questionsDataService.js
@@ -3,211 +3,89 @@
   * Questions data service.
   */
 angular.module('sugoiOverflow.questions')
-  .factory('questionsDataService',
-  function ($http, $q) {
+  .factory('questionsDataService', function (promisedHttp) {
     'use strict'
 
     var service = {
       getAllQuestionsList: function () {
-        var deferred = $q.defer()
-        $http.get('/api/questions/')
-          .success(function (data) {
-            deferred.resolve(data)
-          })
-          .error(function (error) {
-            deferred.reject(error)
-          })
-        return deferred.promise
+        return promisedHttp('get', '/api/questions/')
       },
 
       getSuggestedQuestionsList: function () {
-        var deferred = $q.defer()
-        $http.get('/api/questions/suggested')
-          .success(function (data) {
-            deferred.resolve(data)
-          })
-          .error(function (error) {
-            deferred.reject(error)
-          })
-        return deferred.promise
+        return promisedHttp('get', '/api/questions/suggested')
       },
 
       getMostWantedQuestionsList: function () {
-        var deferred = $q.defer()
-        $http.get('/api/questions/most-wanted')
-          .success(function (data) {
-            deferred.resolve(data)
-          })
-          .error(function (error) {
-            deferred.reject(error)
-          })
-        return deferred.promise
+        return promisedHttp('get', '/api/questions/most-wanted')
       },
 
       getQuestionsAskedByUser: function (username) {
-        var deferred = $q.defer()
-        $http.get(window.s.sprintf('/api/questions/profile/%s/asked', username))
-          .success(function (data) {
-            deferred.resolve(data)
-          })
-          .error(function (error) {
-            deferred.reject(error)
-          })
-        return deferred.promise
+        var url = window.s.sprintf('/api/questions/profile/%s/asked', username)
+        return promisedHttp('get', url)
       },
 
       getQuestionsAnsweredByUser: function (username) {
-        var deferred = $q.defer()
-        $http.get(window.s.sprintf('/api/questions/profile/%s/answered', username))
-          .success(function (data) {
-            deferred.resolve(data)
-          })
-          .error(function (error) {
-            deferred.reject(error)
-          })
-        return deferred.promise
+        var url = window.s.sprintf('/api/questions/profile/%s/answered', username)
+        return promisedHttp('get', url)
       },
 
       getQuestionsSubscribedByUser: function (username) {
-        var deferred = $q.defer()
-        $http.get(window.s.sprintf('/api/questions/profile/%s/subscribed', username))
-          .success(function (data) {
-            deferred.resolve(data)
-          })
-          .error(function (error) {
-            deferred.reject(error)
-          })
-        return deferred.promise
+        var url = window.s.sprintf('/api/questions/profile/%s/subscribed', username)
+        return promisedHttp('get', url)
       },
 
       getQuestionsListSearch: function (terms) {
-        var deferred = $q.defer()
-        $http.get(window.s.sprintf('/api/questions/search/%s', terms))
-          .success(function (data) {
-            deferred.resolve(data)
-          })
-          .error(function (error) {
-            deferred.reject(error)
-          })
-        return deferred.promise
+        var url = window.s.sprintf('/api/questions/search/%s', terms)
+        return promisedHttp('get', url)
       },
 
       getQuestion: function (id) {
-        var deferred = $q.defer()
-        $http.get(window.s.sprintf('/api/questions/one/%s', id))
-          .success(function (data) {
-            deferred.resolve(data)
-          })
-          .error(function (error) {
-            deferred.reject(error)
-          })
-
-        return deferred.promise
+        var url = window.s.sprintf('/api/questions/one/%s', id)
+        return promisedHttp('get', url)
       },
+
       addQuestion: function (question) {
-        var deferred = $q.defer()
-        $http.post('/api/questions/', question)
-          .success(function (addedQuestion) {
-            deferred.resolve(addedQuestion)
-          })
-          .error(function (error) {
-            deferred.reject(error)
-          })
-
-        return deferred.promise
+        return promisedHttp('get', '/api/questions/', question)
       },
+
       subscribeToQuestion: function (questionId) {
-        var deferred = $q.defer()
-        $http.put(window.s.sprintf('/api/questions/%s/subscribe', questionId))
-          .success(function (data) {
-            deferred.resolve(data)
-          })
-          .error(function (error) {
-            deferred.reject(error)
-          })
-
-        return deferred.promise
+        var url = window.s.sprintf('/api/questions/%s/subscribe', questionId)
+        return promisedHttp('put', url)
       },
+
       addAnswer: function (questionId, answer) {
-        var deferred = $q.defer()
+        var url = window.s.sprintf('/api/questions/%s/answer', questionId)
         var data = {
           body: answer
         }
-        $http.post(window.s.sprintf('/api/questions/%s/answer', questionId), data)
-          .success(function (responseData) {
-            deferred.resolve(responseData)
-          })
-          .error(function (error) {
-            deferred.reject(error)
-          })
-
-        return deferred.promise
+        return promisedHttp('post', url, data)
       },
 
       addQuestionComment: function (questionId, comment) {
-        var deferred = $q.defer()
+        var url = window.s.sprintf('/api/questions/%s/comment', questionId)
         var data = {
           body: comment
         }
-
-        $http.post(window.s.sprintf('/api/questions/%s/comment', questionId), data)
-          .success(function (responseData) {
-            deferred.resolve(responseData)
-          })
-          .error(function (error) {
-            deferred.reject(error)
-          })
-        return deferred.promise
+        return promisedHttp('post', url, data)
       },
       addAnswerComment: function (questionId, answerId, comment) {
-        var deferred = $q.defer()
+        var url = window.s.sprintf('/api/questions/%s/answer/%s/comment', questionId, answerId)
         var data = {
           body: comment
         }
-        $http.post(window.s.sprintf('/api/questions/%s/answer/%s/comment', questionId, answerId), data)
-          .success(function (responseData) {
-            deferred.resolve(responseData)
-          })
-          .error(function (error) {
-            deferred.reject(error)
-          })
-        return deferred.promise
+        return promisedHttp('post', url, data)
       },
       upvoteAnswer: function (questionId, answerId) {
-        var deferred = $q.defer()
-        $http.put(window.s.sprintf('/api/questions/%s/answer/%s/upvote', questionId, answerId))
-          .success(function (data) {
-            deferred.resolve(data)
-          })
-          .error(function (error) {
-            deferred.reject(error)
-          })
-
-        return deferred.promise
+        var url = window.s.sprintf('/api/questions/%s/answer/%s/upvote', questionId, answerId)
+        return promisedHttp('put', url)
       },
       downvoteAnswer: function (questionId, answerId) {
-        var deferred = $q.defer()
-        $http.put(window.s.sprintf('/api/questions/%s/answer/%s/downvote', questionId, answerId))
-          .success(function (data) {
-            deferred.resolve(data)
-          })
-          .error(function (error) {
-            deferred.reject(error)
-          })
-
-        return deferred.promise
+        var url = window.s.sprintf('/api/questions/%s/answer/%s/downvote', questionId, answerId)
+        return promisedHttp('put', url)
       },
       markAnswerAsCorrect: function (questionId, answerId) {
-        var deferred = $q.defer()
-        $http.put(window.s.sprintf('/api/questions/%s/answer/%s/correct', questionId, answerId))
-          .success(function (data) {
-            deferred.resolve(data)
-          })
-          .error(function (error) {
-            deferred.reject(error)
-          })
-
-        return deferred.promise
+        var url = window.s.sprintf('/api/questions/%s/answer/%s/correct', questionId, answerId)
+        return promisedHttp('put', url)
       }
     }
     return service
